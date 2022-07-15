0ed697ace337400ea149eed02cdcc8d493ea5563
Kaelen Cook <47254824+ShovelSquid@users.noreply.github.com>
2022-07-15 15:37:41 -0700

    die assets and text assets

--- src/Play.js
+++ src/Play.js
@@ -8,9 +8,32 @@ class Play extends Phaser.Scene {
 
         this.load.image('background', 'paper_background.png');
         this.load.image('player', 'Exclamation_Point.png');
+        this.load.spritesheet('font', 'Alphabet_Spritesheet.png', {
+            frameWidth: 64,
+            frameHeight: 64,
+            startFrame: 0,
+            endFrame: 32,
+        });
+        this.load.spritesheet('negative die', 'Die_Negative_Sheet.png', {
+            frameWidth: 96,
+            frameHeight: 96,
+            startFrame: 1,
+            endFrame: 6
+        });
+        this.load.spritesheet('positive die', 'Die_Positive_Sheet.png', {
+            frameWidth: 96,
+            frameHeight: 96,
+            startFrame: 1,
+            endFrame: 6
+        });
     }
 
     create() {
+        // this.anims.create({
+        //     key: 'fontplayer',
+        //     frames: this.anims.generateFrameNumbers('negative die', {start:1, end: 1}),
+        //     frameRate: 1
+        // })
         this.add.tileSprite(0, 0, this.width, this.height, 'background');
 
         const KeyCodes = Phaser.Input.Keyboard.KeyCodes; // Less typing
